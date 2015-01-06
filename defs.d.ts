09c124188845e69f028b1c4044ce6d7b8ae281d9
Stan Lewis <slewis@fusesource.com>
2015-01-06 10:59:00 -0500

    back out underscore.string, bring back in sugarjs

--- defs.d.ts
+++ defs.d.ts
@@ -3,18 +3,10 @@
 /// <reference path="angular.d.ts"/>
 /// <reference path="angular-resource.d.ts"/>
 /// <reference path="angular-route.d.ts"/>
-
-/// <reference path="underscore.string.d.ts"/>
 /// <reference path="lodash.d.ts"/>
-
-declare module _ {
-  interface LoDashStatic {
-    str: UnderscoreStringStatic;
-    string: UnderscoreStringStatic;
-  }
-}
+/// <reference path="sugar.d.ts"/>
+/// <reference path="jolokia.d.ts"/>
 
 /// <reference path="hawtio-core.d.ts"/>
 /// <reference path="hawtio-core-navigation.d.ts"/>
 
-/// <reference path="jolokia.d.ts"/>
