4d1f5c342f479d90eacd036eea27b2545a24556a
Stan Lewis <slewis@fusesource.com>
2015-01-07 15:59:33 -0500

    Add more defs

--- defs.d.ts
+++ defs.d.ts
@@ -1,12 +1,17 @@
 /// <reference path="logger.d.ts"/>
 /// <reference path="jquery.d.ts"/>
+/// <reference path="toastr.d.ts"/>
+/// <reference path="dynatree.d.ts"/>
 /// <reference path="angular.d.ts"/>
 /// <reference path="angular-resource.d.ts"/>
 /// <reference path="angular-route.d.ts"/>
 /// <reference path="lodash.d.ts"/>
 /// <reference path="sugar.d.ts"/>
 /// <reference path="jolokia.d.ts"/>
+/// <reference path="URI.d.ts"/>
 
 /// <reference path="hawtio-core.d.ts"/>
 /// <reference path="hawtio-core-navigation.d.ts"/>
+
+declare var DTNodeStatus_Ok:any;
 
