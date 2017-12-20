1227ff629f57c505c42ea4e190e49308d0cdd7fd
Alexandre Kieling <abrianik@redhat.com>
2017-12-20 14:25:49 -0200

    add ChildLogger interface to logger.d.ts

--- logger.d.ts
+++ logger.d.ts
@@ -1,4 +1,4 @@
-declare var Logger:Logging.LoggerStatic;
+declare var Logger: Logging.LoggerStatic;
 
 declare module Logging {
 
@@ -8,33 +8,38 @@ declare module Logging {
   }
 
   export interface Logger {
-    setHandler(handler:Function):void;
-    enabledFor(level:LogLevel):boolean;
-    setLevel(level:LogLevel):void;
-    debug(...arguments:any[]):void;
-    info(...arguments:any[]):void;
-    warn(...arguments:any[]):void;
-    error(...arguments:any[]):void;
-    formatStackTraceString(stack:string):string;
+    setHandler(handler: Function): void;
+    enabledFor(level: LogLevel): boolean;
+    setLevel(level: LogLevel): void;
+    debug(...arguments: any[]): void;
+    info(...arguments: any[]): void;
+    warn(...arguments: any[]): void;
+    error(...arguments: any[]): void;
+    formatStackTraceString(stack: string): string;
   }
 
   export interface LoggerStatic extends Logger {
-    useDefaults(level:LogLevel):void;
-    get(name:string):Logger;
-    oldGet(name:string):Logger;
-    getType(obj:any):string;
-    isError(obj:any):boolean;
-    isArray(obj:any):boolean;
-    isObject(obj:any):boolean;
-    isString(obj:any):boolean;
-    DEBUG:LogLevel;
-    INFO:LogLevel;
-    WARN:LogLevel;
-    ERROR:LogLevel;
-    OFF:LogLevel;
-    storagePrefix:string;
-    loggers:Object;
+    useDefaults(level: LogLevel): void;
+    get(name: string): Logger;
+    oldGet(name: string): Logger;
+    getType(obj: any): string;
+    isError(obj: any): boolean;
+    isArray(obj: any): boolean;
+    isObject(obj: any): boolean;
+    isString(obj: any): boolean;
+    DEBUG: LogLevel;
+    INFO: LogLevel;
+    WARN: LogLevel;
+    ERROR: LogLevel;
+    OFF: LogLevel;
+    storagePrefix: string;
+    loggers: Object;
 
+  }
+
+  export interface ChildLogger {
+    name: string;
+    filterLevel: Logging.LogLevel;
   }
 
 }
