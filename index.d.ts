2dc4173187d4e5335c41bb50e9d1e4a2d2b04c08
Antonin Stefanutti <antonin@stefanutti.fr>
2018-09-07 11:05:19 +0200

    chore: Remove Jolokia TS definition

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="logger.d.ts"/>
 /// <reference path="toastr.d.ts"/>
-/// <reference path="jolokia.d.ts"/>
 /// <reference path="codemirror.d.ts"/>
 /// <reference path="codemirror-additional.d.ts"/>
 /// <reference path="gridster.d.ts"/>
